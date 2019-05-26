2c8cbeb0a0d8dbd3c20b94d3ce37a9e42b7d7526
bryan.jsb <bryan.jsb.1801@gmail.com>
2019-05-26 10:37:38 -0600

    e

--- Server/Cartas/CartaTipo.cs
+++ Server/Cartas/CartaTipo.cs
@@ -6,26 +6,26 @@ namespace Servidor.Cartas
 {
     public enum numCarta
     {
-        Two = 2,
-        Three = 3,
-        Four = 4,
-        Five = 5,
-        Six = 6,
-        Seven = 7,
-        Eight = 8,
-        Nine = 9,
-        Ten = 10,
-        Jack = 11,
-        Queen = 12,
-        King = 13,
-        Ace = 14,
+        Dos = 2,
+        Tres = 3,
+        Cuatro = 4,
+        Cinco = 5,
+        Seis = 6,
+        Siete = 7,
+        Ocho = 8,
+        Nueve = 9,
+        Diez = 10,
+        Jota = 11,
+        Reina = 12,
+        Rey = 13,
+        As = 14,
     }
 
     public enum tipoCarta
     {
-        Club = 0, // ♣
-        Diamond = 1, // ♦
-        Heart = 2, // ♥
-        Spade = 3 // ♠
+        Trebol = 0, // ♣
+        Diamante = 1, // ♦
+        Corazon = 2, // ♥
+        Espada = 3 // ♠
     }
 }
---
+++ Server/Cartas/Mazo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servidor.Cartas
+{
+    class Mazo
+    {
+        public Mazo() { }
+        private void generarMazo() { }
+    }
+}
