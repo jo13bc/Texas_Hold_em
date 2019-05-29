87c332ec5e362395d0e10f6811c5009008f856c7
LizaMarie <lizamariejimenez@gmail.com>
2019-05-29 15:10:42 -0600

    empezando el grafico

---
+++ Class2.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Jugador
+{
+	public Class1()
+	{
+	}
+}
---
+++ Cliente/Jugadores/DatosJugadores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente.Jugadores
+{
+    abstract class DatosJugadores: Jugador 
+    {
+        public abstract string nombre { get; }
+
+        public abstract int buyin { get; }
+    }
+}
