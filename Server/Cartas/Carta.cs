80c930f114beb1b707850e86f75dc38f1f1fa4e8
bryan.jsb <bryan.jsb.1801@gmail.com>
2019-05-29 21:32:57 -0600

    logica carta

---
+++ Server/Cartas/Carta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servidor.Cartas
+{
+
+    class Carta
+    {
+
+        Rango valorCarta;
+        Palo paloCarta;
+        bool bocaArriba;
+        public enum Rango
+        {
+            Dos = 2, Tres = 3, Cuatro = 4, Cinco = 5, Seis = 6, Siete = 7, Ocho = 8, Nueve = 9, Diez = 10,
+            Jota = 11, Reina = 12, Rey = 13, As = 14,
+        }
+
+        public enum Palo
+        {
+            Trebol = 0, // ♣
+            Diamante = 1, // ♦
+            Corazon = 2, // ♥
+            Espada = 3 // ♠
+        }
+
+        Carta() {
+            valorCarta = Rango.As ;
+            paloCarta = Palo.Corazon;
+            bocaArriba = false;
+        }
+        Carta(Rango r, Palo p, bool b)
+        {
+            valorCarta = r;
+            paloCarta = p;
+            bocaArriba = b;
+        }
+
+        ~Carta(){ }
+
+
+        Rango getValor() { return valorCarta; }
+        Palo getPalo() { return paloCarta; }
+        bool estaBocaArriba() { return bocaArriba; }
+
+        void setValor(Rango r) { valorCarta = r; }
+        void setPalo(Palo p) { paloCarta = p; }
+        void voltear() { bocaArriba = !bocaArriba; }
+    }
+ }
+
+      
+   
+
+
+
+     
+	
+
