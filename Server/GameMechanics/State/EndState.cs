00f8f7d453341ad350adbeca5f894409c1ed3b0b
bryan.jsb <bryan.jsb.1801@gmail.com>
2019-06-10 23:49:53 -0600

    EndState

--- Server/GameMechanics/State/EndState.cs
+++ Server/GameMechanics/State/EndState.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿using Servidor.Cartas;
+using Servidor.Configurations;
+using Servidor.GameMechanics.MachineState;
+using Servidor.Hand;
+using Servidor.Jugador;
+using Servidor.Logic.Player;
+using Servidor.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using static Servidor.Utility.TexasHoldem.TexasHoldEmUtil;
+
 
 namespace Servidor.GameMechanics.State
 {
-    class EndState
+    class EndState : IState<ModelContext>
     {
+
+        public static readonly String NAME = "EndHand";
+        public bool execute(ModelContext context)
+        {
+
+            PlayerEntity dealearPlayer = context.getPlayer(context.getDealer());
+            List<PlayerEntity> players = context.getPlayers();
+            
+            List<PlayerEntity> nextPlayers = new List<PlayerEntity>(players.Count);
+
+            int dealerIndex = 0;
+            int i = 0;
+
+            foreach (PlayerEntity p in players){
+                if (p.getChips()>0) {
+                    nextPlayers.Add(p);
+                    i++;
+                }
+
+                if (dealearPlayer==p)
+                {
+                    dealerIndex = i - 1;
+                }
+
+
+               
+            }
+            
+            context.setDealer(dealerIndex);
+            context.setPlayers(nextPlayers);
+            return true;
+
+        }
+
+        public string getName()
+        {
+            return NAME;
+        }
     }
 }
