769d1d3ef61a65b31f4dd9e093149a774d08853a
bryan.jsb <bryan.jsb.1801@gmail.com>
2019-06-11 04:47:29 -0600

    Update GameController.cs

--- Server/Controller/GameController.cs
+++ Server/Controller/GameController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using Servidor.Configurations;
+using Servidor.Events;
+using Servidor.GameMechanics;
 using Servidor.Jugador;
 
 namespace Servidor.Controller
@@ -13,24 +15,41 @@ namespace Servidor.Controller
         private static readonly int DISPATCHER_THREADS = 1;
         private static readonly int EXTRA_THREADS = 2;
         public static readonly String SYSTEM_CONTROLLER = "system";
-   
-    private readonly Dictionary<String, IGameEventDispatcher<PokerEventType>> players = new HashMap<>();
-        private readonly List<String> playersByName = new ArrayList<>();
-        private readonly List<ExecutorService> subExecutors = new ArrayList<>();
-        private readonly Map<PokerEventType, IGameEventProcessor<PokerEventType, IStrategy>> playerProcessors;
-        private readonly GameEventDispatcher<ConnectorGameEventType, StateMachineConnector> connectorDispatcher;
+
+        public static readonly String INIT_HAND_EVENT_TYPE = "initHand";
+        public static readonly String BET_COMMAND_EVENT_TYPE = "initHand";
+
+        public static readonly String END_GAME_PLAYER_EVENT_TYPE = "endGame";
+        public static readonly String END_HAND_PLAYER_EVENT_TYPE = "endHand";
+
+        public static readonly String CHECK_PLAYER_EVENT_TYPE = "check";
+
+        public static readonly String GET_COMMAND_PLAYER_EVENT_TYPE = "getCommand";
+
+        public static readonly String EXIT_CONNECTOR_EVENT_TYPE = "exit";
+        public static readonly String ADD_PLAYER_CONNECTOR_EVENT_TYPE = "addPlayer";
+        public static readonly String TIMEOUT_CONNECTOR_EVENT_TYPE = "timeOutCommand";
+        public static readonly String CREATE_GAME_CONNECTOR_EVENT_TYPE = "createGame";
+
+        private readonly Dictionary<String, IGameEventDispatcher> players = new Dictionary<string, IGameEventDispatcher>();
+        private readonly List<String> playersByName = new List<string>();
+        private readonly Dictionary<String, IGameEventProcessor<IStrategy>> playerProcessors = new Dictionary<String, IGameEventProcessor<IStrategy>>();
+
+        private readonly GameEventDispatcher<StateMachineConnector> connectorDispatcher;
         private readonly StateMachineConnector stateMachineConnector;
-    private readonly IGameTimer timer;
-    private Settings settings;
-        private ExecutorService executors;
-        private boolean finish = false;
+
+        private Settings settings;
+
+        private ExecutorService executors;        
+        private List<ExecutorService> subExecutors = new List<ExecutorService>();
 
 
         public GameController()
-           
         {
-
-
+            stateMachineConnector = new StateMachineConnector(players);
+            connectorDispatcher = new GameEventDispatcher<StateMachineConnector>(StateMachineConnector,buildConnectorProcessors(),buildExecutor());
+            stateMachineConnector.setSystem(connectorDispatcher);
+            playerProcessors = buildPlayerProcessors();
         }
 
         public bool addStrategy(IStrategy strategy)
